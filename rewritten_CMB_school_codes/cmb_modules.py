40b8757b148355254909a689d1ba2a2266007406
masterdesky <masterdesky@gmail.com>
2020-09-24 21:37:41 +0200

    rewritten first notebooks

--- rewritten_CMB_school_codes/cmb_modules.py
+++ rewritten_CMB_school_codes/cmb_modules.py
@@ -8,59 +8,67 @@ import astropy.io.fits as fits
 
 
 
-def make_CMB_T_map(N,pix_size,ell,DlTT):
-    "makes a realization of a simulated CMB sky map given an input DlTT as a function of ell,"
-    "the pixel size (pix_size) required and the number N of pixels in the linear dimension."
-    #np.random.seed(100)
-    # convert Dl to Cl
-    ClTT = DlTT * 2 * np.pi / (ell*(ell+1.))
-    ClTT[0] = 0. # set the monopole and the dipole of the Cl spectrum to zero
-    ClTT[1] = 0.
+def make_CMB_T_map(ell, DlTT,
+                   N=2**10, pix_size=0.5):
+    """
+    Makes a realization of a simulated CMB sky map given an input `DlTT` as a function of `ell`.
     
-    # make a 2D real space coordinate system
+    Parameters:
+    -----------
+    ell : array-like
+        desc
+
+    DlTT : array-like
+        desc
+    
+    N : int
+        Number of pixel in the linear dimension
+        
+    pix_size : float
+        Size of a pixel in arcminutes
+    """
+
+    # Convert Dl to Cl
+    ClTT = DlTT * 2 * np.pi / (ell * (ell + 1))
+    # Set the monopole and the dipole of the Cl spectrum to zero
+    ClTT[0] = 0
+    ClTT[1] = 0
+
+    # Make a 2D real space coordinate system
     onesvec = np.ones(N)
-    inds  = (np.arange(N)+.5 - N/2.) /(N-1.) # create an array of size N between -0.5 and +0.5
-    # compute the outer product matrix: X[i, j] = onesvec[i] * inds[j] for i,j
-    # in range(N), which is just N rows copies of inds - for the x dimension
-    X = np.outer(onesvec,inds)
-    # compute the transpose for the y dimension
+    inds  = (np.arange(N) + 0.5 - N/2) /(N - 1) # create an array of size N between -0.5 and +0.5
+    # Compute the outer product matrix: X[i, j] = onesvec[i] * inds[j] for i,j 
+    # in range(N), which is just `N` rows copies of inds - for the x dimension
+    X = np.outer(onesvec, inds) 
+    # Compute the transpose for the y dimension
     Y = np.transpose(X)
-    # radial component R
-    R = np.sqrt(X**2. + Y**2.)
+    # Radial component `R`
+    R = np.sqrt(X**2 + Y**2)
     
-    # now make a 2D CMB power spectrum
-    pix_to_rad = (pix_size/60. * np.pi/180.) # going from pix_size in arcmins to degrees and then degrees to radians
-    ell_scale_factor = 2. * np.pi /pix_to_rad  # now relating the angular size in radians to multipoles
-    ell2d = R * ell_scale_factor # making a fourier space analogue to the real space R vector
-    ClTT_expanded = np.zeros(int(ell2d.max())+1)
-    # making an expanded Cl spectrum (of zeros) that goes all the way to the size of the 2D ell vector
-    ClTT_expanded[0:(ClTT.size)] = ClTT # fill in the Cls until the max of the ClTT vector
+    # Now make a 2D CMB power spectrum
+    pix_to_rad = (pix_size/60 * np.pi/180)     # Going from `pix_size` in arcmins to degrees and then degrees to radians
+    ell_scale_factor = 2 * np.pi / pix_to_rad  # Now relating the angular size in radians to multipoles
+    ell2d = R * ell_scale_factor               # Making a fourier space analogue to the real space R vector
+    ClTT_expanded = np.zeros(int(ell2d.max()) + 1) 
+    # Making an expanded Cl spectrum (of zeros) that goes all the way to the size of the 2D ell vector
+    ClTT_expanded[0:(ClTT.size)] = ClTT        # Fill in the Cls until the max of the ClTT vector
+
+    # The 2D Cl spectrum is defined on the multiple vector set by the pixel scale
+    CLTT2d = ClTT_expanded[ell2d.astype(int)] 
     
-    # the 2D Cl spectrum is defined on the multiple vector set by the pixel scale
-    CLTT2d = ClTT_expanded[ell2d.astype(int)]
-    #plt.imshow(np.log(CLTT2d))
-    
-    
-    # now make a realization of the CMB with the given power spectrum in real space
+    # Now make a realization of the CMB with the given power spectrum in real space
     random_array_for_T = np.random.normal(0,1,(N,N))
-    FT_random_array_for_T = np.fft.fft2(random_array_for_T)   # take FFT since we are in Fourier space
+    FT_random_array_for_T = np.fft.fft2(random_array_for_T)   # Take FFT since we are in Fourier space
+    FT_2d = np.sqrt(CLTT2d) * FT_random_array_for_T           # We take the sqrt since the power spectrum is T^2
     
-    FT_2d = np.sqrt(CLTT2d) * FT_random_array_for_T # we take the sqrt since the power spectrum is T^2
-    plt.imshow(np.real(FT_2d))
-    
-    
-    ## make a plot of the 2D cmb simulated map in Fourier space, note the x and y axis labels need to be fixed
-    #Plot_CMB_Map(np.real(np.conj(FT_2d)*FT_2d*ell2d * (ell2d+1)/2/np.pi),0,np.max(np.conj(FT_2d)*FT_2d*ell2d * (ell2d+1)/2/np.pi),ell2d.max(),ell2d.max())  ###
-    
-    # move back from ell space to real space
-    CMB_T = np.fft.ifft2(np.fft.fftshift(FT_2d))
-    # move back to pixel space for the map
+    # Move back from ell space to real space
+    CMB_T = np.fft.ifft2(np.fft.fftshift(FT_2d)) 
+    # Move back to pixel space for the map
     CMB_T = CMB_T/(pix_size /60.* np.pi/180.)
-    # we only want to plot the real component
+    # We only want to plot the real component
     CMB_T = np.real(CMB_T)
-    
-    ## return the map
-    return(CMB_T)
+
+    return(CMB_T, CLTT2d, FT_2d, ell2d)
 ###############################
 
 def Plot_CMB_Map(Map_to_Plot,c_min,c_max,X_width,Y_width):
